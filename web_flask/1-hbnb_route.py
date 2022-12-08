b818cf5b17a764a2dfbdb58d4bf727447ecff23e
kezzyj <karachidavid31@gmail.com>
2022-12-08 08:25:48 -0800

    style validation

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -15,6 +15,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
 
@@ -22,8 +23,8 @@ def hello_hbnb():
 
     return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes=False)
 
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
 
     """Displays 'HBNB'."""
