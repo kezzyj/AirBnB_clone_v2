f6dcd7515f45a0bb3a9ae54dcc549ed9da4b9529
kezzyj <karachidavid31@gmail.com>
2022-12-08 19:59:42 -0800

    editing database to close and city to return list of cities

--- models/state.py
+++ models/state.py
@@ -11,8 +11,8 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-    	cascade="all, delete, delete-orphan")
+    cities = relationship("City", backref="state", 
+		cascade="all, delete, delete-orphan")
 
     if getenv('HBNB_TYPE_STORAGE') != "db":
         @property
