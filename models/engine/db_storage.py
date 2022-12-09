75c7138fdd3c27b1c11a2477ece3a07564ea0cb3
kezzyj <karachidavid31@gmail.com>
2022-12-09 07:18:58 -0800

    fixing syntax error

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""This module defines a class to manage db storage for hbnb clone"""
+"""Defines the DBStorage engine."""
 from os import getenv
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker, scoped_session
-import models
 from models.base_model import Base
 from models.base_model import BaseModel
 from models.amenity import Amenity
@@ -12,6 +9,10 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
 
 
 class DBStorage:
